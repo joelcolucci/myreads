a53834eda3c3c57a0e4b34f65c83ccd761b5c4a0
joelcolucci <joelcolucci@gmail.com>
2018-02-28 10:02:23 -0500

    feat: implement SearchBooksBar as controlled component

--- src/SearchBooksBar.js
+++ src/SearchBooksBar.js
@@ -2,26 +2,46 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-function SearchBooksBar(props) {
-  return (
-    <div className="search-books-bar">
-      <Link
-        to="/"
-        className="close-search">Close</Link>
-      <div className="search-books-input-wrapper">
-        {/*
-          NOTES: The search from BooksAPI is limited to a particular set of search terms.
-          You can find these search terms here:
-          https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class SearchBooksBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-          However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-          you don't find a specific author or title. Every search is limited by search terms.
-        */}
-        <input type="text" placeholder="Search by title or author"/>
+    this.state = {
+      query: ''
+    };
 
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+  }
+
+  handleQueryChange(event) {
+    let query = event.target.value;
+    this.setState({ query: query.trim() });
+  }
+
+  render() {
+    return (
+      <div className="search-books-bar">
+        <Link
+          to="/"
+          className="close-search">Close</Link>
+        <div className="search-books-input-wrapper">
+          {/*
+            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+            You can find these search terms here:
+            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+  
+            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+            you don't find a specific author or title. Every search is limited by search terms.
+          */}
+          <input
+            value={this.state.query}
+            onChange={this.handleQueryChange}
+            type="text"
+            placeholder="Search by title or author"/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 
