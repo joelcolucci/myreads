4e478092968d635e3442a5d1f42370a750f900c4
joelcolucci <joelcolucci@gmail.com>
2018-02-27 20:28:46 -0500

    refactor: factor down SearchBooks into stateless functional components

---
+++ src/SearchBooksResults.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function SearchBooksResults(props) {
+  return (
+    <div className="search-books-results">
+      <ol className="books-grid"></ol>
+    </div>
+  );
+}
+
+
+export default SearchBooksResults;
