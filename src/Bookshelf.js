0b15790ddf64d16b5700bf8f2e357d3bd5219064
joelcolucci <joelcolucci@gmail.com>
2018-02-27 21:52:02 -0500

    feat: implement ajax load, filter, render of books

---
+++ src/Bookshelf.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Book from './Book';
+
+
+function Bookshelf(props) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map((book) => {
+            return (
+              <li key={book.id}>
+                <Book
+                  title={book.title}
+                  author={book.author}
+                  coverUrl={book.imageLinks.thumbnail} />
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+
+Bookshelf.propTypes = {
+  title: PropTypes.string,
+  books: PropTypes.array
+};
+
+
+export default Bookshelf;
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,21 +1,65 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import * as BooksAPI from './BooksAPI';
+import Bookshelf from './Bookshelf';
 import ListBooksTitle from './ListBooksTitle';
 import ListBooksContent from './ListBooksContent';
 
 
-function ListBooks(props) {
- return (
-  <div className="list-books">
-    <ListBooksTitle />
-    <ListBooksContent />
-    <div className="open-search">
-      <Link
-        to="/search">Add a book</Link>
-    </div>
-  </div>
- )
+class ListBooks extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    BooksAPI
+      .getAll()
+      .then((books) => this.setState({
+        books: books
+      }));
+  }
+
+  render() {
+    let books = this.state.books;
+
+    let currentlyReadingBooks = books.filter((book) => {
+      return book.shelf === 'currentlyReading';
+    });
+
+    let wantToReadBooks = books.filter((book) => {
+      return book.shelf === 'wantToRead';
+    });
+
+    let readBooks = books.filter((book) => {
+      return book.shelf === 'read';
+    });
+
+    return (
+      <div className="list-books">
+        <ListBooksTitle />
+        <ListBooksContent>
+          <Bookshelf
+            title="Currently Reading"
+            books={currentlyReadingBooks} />
+          <Bookshelf
+            title="Want to Read"
+            books={wantToReadBooks} />
+          <Bookshelf
+            title="Read"
+            books={readBooks} />
+        </ListBooksContent>
+        <div className="open-search">
+          <Link
+            to="/search">Add a book</Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ListBooks;
