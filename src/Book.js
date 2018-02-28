0631a1f8da465d7d8a5c2acaed268d22023daa2b
joelcolucci <joelcolucci@gmail.com>
2018-02-28 08:59:15 -0500

    refactor: convert Book to a controlled component

--- src/Book.js
+++ src/Book.js
@@ -2,35 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-function Book(props) {
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{
-          width: 128,
-          height: 193,
-          backgroundImage: `url(${props.coverUrl})`
-          }}></div>
-        <div className="book-shelf-changer">
-          <select>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+class Book extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shelf: 'none'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    let updatedShelf = event.target.value;
+    this.setState({shelf: updatedShelf});
+
+    // TODO: Invoke passed down ListBooks state handler
+  }
+
+  render() {
+    let { title, authors, coverUrl } = this.props;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${coverUrl})`
+            }}></div>
+          <div className="book-shelf-changer">
+            <select value={this.state.shelf} onChange={this.handleChange}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{authors.map((author) => `${author} `)}</div>
       </div>
-      <div className="book-title">{props.title}</div>
-      <div className="book-authors">{props.authors.map((author) => `${author} `)}</div>
-    </div>
-  );
+    );
+  }
 }
 
 
 Book.propTypes = {
   title: PropTypes.string,
-  authors: PropTypes.string,
+  authors: PropTypes.array,
   coverUrl: PropTypes.string
 };
 
