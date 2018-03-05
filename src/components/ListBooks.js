241e52e8495145c03ea83885b27fa49ed30df031
joelcolucci <joelcolucci@gmail.com>
2018-03-05 11:05:53 -0500

    refactor: move books state up to App component

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,91 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import * as BooksAPI from '../utilities/BooksAPI';
 import Bookshelf from './Bookshelf';
 import ListBooksTitle from './ListBooksTitle';
 import ListBooksContent from './ListBooksContent';
 
 
-class ListBooks extends React.Component {
-  constructor(props) {
-    super(props);
+function ListBooks(props) {
+  let { books, onBookshelfUpdate } = props;
 
-    this.state = {
-      books: []
-    };
+  let currentlyReadingBooks = books.filter((book) => {
+    return book.shelf === 'currentlyReading';
+  });
 
-    this.handleBookshelfUpdate = this.handleBookshelfUpdate.bind(this);
-  }
+  let wantToReadBooks = books.filter((book) => {
+    return book.shelf === 'wantToRead';
+  });
 
-  componentDidMount() {
-    BooksAPI
-      .getAll()
-      .then((books) => {
-        this.setState({
-          books: books
-        });
-      });
-  }
+  let readBooks = books.filter((book) => {
+    return book.shelf === 'read';
+  });
 
-  handleBookshelfUpdate(book, shelf) {
-    let updatedBooks = this.state.books.map((item) => {
-      // Use spread syntax to ensure immutability
-      if (item.id === book.id) {
-        return {...item, shelf: shelf};
-      } else {
-        return {...item};
-      }
-    });
-
-    BooksAPI
-      .update(book, shelf)
-      .then((_) => {
-        this.setState({
-          books: updatedBooks
-        });
-      });
-  }
-
-  render() {
-    let books = this.state.books;
-
-    let currentlyReadingBooks = books.filter((book) => {
-      return book.shelf === 'currentlyReading';
-    });
-
-    let wantToReadBooks = books.filter((book) => {
-      return book.shelf === 'wantToRead';
-    });
-
-    let readBooks = books.filter((book) => {
-      return book.shelf === 'read';
-    });
-
-    return (
-      <div className="list-books">
-        <ListBooksTitle />
-        <ListBooksContent>
-          <Bookshelf
-            title="Currently Reading"
-            books={currentlyReadingBooks}
-            onBookshelfUpdate={this.handleBookshelfUpdate} />
-          <Bookshelf
-            title="Want to Read"
-            books={wantToReadBooks}
-            onBookshelfUpdate={this.handleBookshelfUpdate} />
-          <Bookshelf
-            title="Read"
-            books={readBooks}
-            onBookshelfUpdate={this.handleBookshelfUpdate} />
-        </ListBooksContent>
-        <div className="open-search">
-          <Link
-            to="/search">Add a book</Link>
-        </div>
+  return (
+    <div className="list-books">
+      <ListBooksTitle />
+      <ListBooksContent>
+        <Bookshelf
+          title="Currently Reading"
+          books={currentlyReadingBooks}
+          onBookshelfUpdate={onBookshelfUpdate} />
+        <Bookshelf
+          title="Want to Read"
+          books={wantToReadBooks}
+          onBookshelfUpdate={onBookshelfUpdate} />
+        <Bookshelf
+          title="Read"
+          books={readBooks}
+          onBookshelfUpdate={onBookshelfUpdate} />
+      </ListBooksContent>
+      <div className="open-search">
+        <Link
+          to="/search">Add a book</Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default ListBooks;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import * as BooksAPI from '../utilities/BooksAPI';
 import ListBooks from '../components/ListBooks';
 import SearchBooks from '../components/SearchBooks';
 
 
 class BooksApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+      searchResults: []
+    };
+
+    this.handleBookshelfUpdate = this.handleBookshelfUpdate.bind(this);
+  }
+
+  componentDidMount() {
+    BooksAPI
+      .getAll()
+      .then((books) => {
+        this.setState({
+          ...this.state,
+          books: books
+        });
+      });
+  }
+
+  handleBookshelfUpdate(book, shelf) {
+    let updatedBooks = this.state.books.map((item) => {
+      // Use spread syntax to ensure immutability
+      if (item.id === book.id) {
+        return {...item, shelf: shelf};
+      } else {
+        return {...item};
+      }
+    });
+
+    BooksAPI
+      .update(book, shelf)
+      .then((_) => {
+        this.setState({
+          books: updatedBooks
+        });
+      });
+  }
+
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-          <SearchBooks />
+        <Route exact path="/" render={() => (
+          <ListBooks
+            books={this.state.books}
+            onBookshelfUpdate={this.handleBookshelfUpdate} />
         )} />
 
-        <Route exact path="/" render={() => (
-          <ListBooks />
+        <Route path="/search" render={() => (
+          <SearchBooks />
         )} />
       </div>
     );
   }
 }
+
 
 export default BooksApp;
